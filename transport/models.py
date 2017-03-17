8b51f0fc0190ee89d5b221b5c4cf0fd68b64459b
WaltonSimons <masteroskar@gmail.com>
2017-03-17 10:28:46 +0100

    New models added

--- transport/models.py
+++ transport/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
 
@@ -8,19 +9,26 @@ class SiteUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.TextField(max_length=255)
     surname = models.TextField(max_length=255)
-    phone_number = models.TextField(max_length=15)
+    phone_number = models.TextField(max_length=15, blank=True, null=True)
+    company = models.OneToOneField('Company', blank=True, null=True)
+    street = models.TextField(max_length=255, blank=True, null=True)
+    postcode = models.IntegerField(blank=True, null=True)
+    city = models.ForeignKey('City', blank=True, null=True)
 
 
-#    company = models.OneToOneField(Company, on_delete=models.CASCADE(), blank=True, null=True)
-#    street = models.TextField(max_length=255)
-#    postcode = models.IntegerField(max_length=5)
-#    city = models.OneToOneField()
-#
-#
-#class Company(models.Model):
-#    name = models.TextField(max_length=255)
-#    nip = models.IntegerField(max_length=10)
-#
-#
-#class City(models.Model):
-#    name = models.TextField(max_length=255)
+class Company(models.Model):
+    name = models.TextField(max_length=255)
+    nip = models.IntegerField()
+    street = models.TextField(max_length=255, blank=True, null=True)
+    postcode = models.IntegerField(blank=True, null=True)
+    city = models.ForeignKey('City', blank=True, null=True)
+
+
+class City(models.Model):
+    name = models.TextField(max_length=255, primary_key=True)
+
+
+class Offer(models.Model):
+    creation_date = models.DateTimeField(default=datetime.now())
+    title = models.TextField(max_length=512)
+    description = models.TextField(max_length=5000)
