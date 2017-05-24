fd1a50c4e32b12b84d8520352ccd71435a2c08f2
WaltonSimons <oskarswiatkowski@gmail.com>
2017-05-24 10:55:59 +0200

    Refactored match value calculating to support multiple values with weights.

--- transport/offer_matching.py
+++ transport/offer_matching.py
@@ -3,9 +3,14 @@ from .maps import distance_between_locations
 
 
 def calculate_match_value(offer, user):
+    # values is a list of tuples - (value, weight)
+    values = []
     distance = distance_between_locations(offer.start_location, user.location)
     p_distance = (1.0/(1.0+distance/100.0))
-    return p_distance
+    values.append((p_distance, 1))
+
+    res = sum([x * y for x, y in values]) / sum([y for x, y in values])
+    return res
 
 
 def create_match(offer, user):
