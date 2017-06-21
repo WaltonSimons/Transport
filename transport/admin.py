47ca58de27500dcb4b34dadde519f40c00fa4442
WaltonSimons <masteroskar@gmail.com>
2017-06-21 15:48:41 +0200

    Admin panel models added.

--- transport/admin.py
+++ transport/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import SiteUser, Conversation, Offer, Location
+from .models import *
 
 # Register your models here.
 
@@ -7,3 +7,13 @@ admin.site.register(SiteUser)
 admin.site.register(Conversation)
 admin.site.register(Offer)
 admin.site.register(Location)
+admin.site.register(Preferences)
+admin.site.register(Company)
+admin.site.register(Vehicle)
+admin.site.register(CargoType)
+admin.site.register(Category)
+admin.site.register(UserRating)
+admin.site.register(OfferMatch)
+admin.site.register(OfferBid)
+admin.site.register(Message)
+admin.site.register(Filter)
